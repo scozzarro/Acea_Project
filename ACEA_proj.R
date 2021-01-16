15f4cd104db302481f4b85aebe79ab713bee45e6
Gabriel <g.scozzarro@gmail.com>
2021-01-16 17:51:41 +0100

    add depth to underground SAL analysis and prediction model

--- ACEA_proj.R
+++ ACEA_proj.R
@@ -20,11 +20,19 @@ vis_miss(data)
 # 3.0 Data preparation ----
 
 na_indexes<- which(is.na(data$Depth_to_Groundwater_LT2))
+na_indexes_SAL<- which(is.na(data$Depth_to_Groundwater_SAL))
+
 
 full_data_LT2<- data[-na_indexes,]
+full_data_SAL<- data[-na_indexes_SAL,]
 
-full_data_LT2<- full_data_LT2 %>% rename(Date = �..Date)
+
+full_data_LT2<- full_data_LT2 %>% rename(Date = ï..Date)
 full_data_LT2$Date<- as.Date(full_data_LT2$Date, format = c('%d/%m/%Y'))
+
+full_data_SAL<- full_data_SAL %>% rename(Date = ï..Date)
+full_data_SAL$Date<- as.Date(full_data_SAL$Date, format = c('%d/%m/%Y'))
+
 
 # 4.0 EDA ----
 full_data_LT2 %>% select(Date, Depth_to_Groundwater_LT2) %>%
@@ -39,23 +47,49 @@ LT2_outliers <- boxplot.stats(full_data_LT2$Depth_to_Groundwater_LT2)$out
 LT2_outliers_indexes <- which(full_data_LT2$Depth_to_Groundwater_LT2%in% LT2_outliers)
 full_data_LT2<- full_data_LT2[-LT2_outliers_indexes,]
 
+full_data_SAL %>% select(Date, Depth_to_Groundwater_SAL) %>%
+                  plot_time_series(Date, Depth_to_Groundwater_SAL, .title = 'SAL Ground Water trend over time')
+
+full_data_SAL %>% ggplot(aes(Depth_to_Groundwater_SAL)) +
+                  geom_boxplot() +
+                  coord_flip() +
+                  ggtitle('SAL Ground Water values')
+
+SAL_outliers <- boxplot.stats(full_data_SAL$Depth_to_Groundwater_SAL)$out
+SAL_outliers_indexes <- which(full_data_SAL$Depth_to_Groundwater_SAL %in% SAL_outliers)
+full_data_SAL<- full_data_SAL[-SAL_outliers_indexes,]
+
 # 5.0 ML Preparation ----
 forecast_horizon<- 360
 
 full_data_LT2_tbl<- full_data_LT2 %>% 
                     select(Date, Depth_to_Groundwater_LT2) %>%
                     future_frame(.date_var = Date, .length_out = forecast_horizon, .bind_data = TRUE)
+
+full_data_SAL_tbl<- full_data_SAL %>% 
+                    select(Date, Depth_to_Groundwater_SAL) %>%
+                    future_frame(.date_var = Date, .length_out = forecast_horizon, .bind_data = TRUE)
+
 #training Data
 train_LT2_tbl<- full_data_LT2_tbl %>% filter(!is.na(Depth_to_Groundwater_LT2))
 train_LT2_tbl %>% tk_summary_diagnostics()
+
+train_SAL_tbl<- full_data_SAL_tbl %>% filter(!is.na(Depth_to_Groundwater_SAL))
+train_SAL_tbl %>% tk_summary_diagnostics()
 
 #Future Data
 future_LT2_tbl<- full_data_LT2_tbl %>% filter(is.na(Depth_to_Groundwater_LT2))
 future_LT2_tbl %>% tk_summary_diagnostics()
 
+future_SAL_tbl<- full_data_SAL_tbl %>% filter(is.na(Depth_to_Groundwater_SAL))
+future_SAL_tbl %>% tk_summary_diagnostics()
+
 #time splits
 splits<- train_LT2_tbl %>% time_series_split(date_var = Date, assess = forecast_horizon, cumulative = TRUE)
 splits
+
+splits_SAL<- train_SAL_tbl %>% time_series_split(date_var = Date, assess = forecast_horizon, cumulative = TRUE)
+splits_SAL
 
 # Preprocessing
 recipe_1<- recipe(Depth_to_Groundwater_LT2 ~., training(splits)) %>%
@@ -65,11 +99,24 @@ recipe_1<- recipe(Depth_to_Groundwater_LT2 ~., training(splits)) %>%
            step_mutate(Date_day = factor(Date_day, ordered = TRUE)) %>%
            step_dummy(all_nominal(), one_hot = TRUE)
 
-recipe_1 %>% prep() %>% juice() %>% glimpse()          
+recipe_1_SAL<- recipe(Depth_to_Groundwater_SAL ~., training(splits_SAL)) %>%
+               step_timeseries_signature(Date) %>%
+               step_rm(matches('(.iso)|(.xts$)|(hour)|(minute)|(second)|(am.pm)')) %>%
+               step_normalize(Date_index.num, Date_year) %>%
+               step_mutate(Date_day = factor(Date_day, ordered = TRUE)) %>%
+               step_dummy(all_nominal(), one_hot = TRUE)
+
+recipe_1 %>% prep() %>% juice() %>% glimpse()
+recipe_1_SAL %>% prep() %>% juice() %>% glimpse()
+
 recipe_2<- recipe_1 %>% update_role(Date, new_role = 'ID')
+recipe_2_SAL<- recipe_1_SAL %>% update_role(Date, new_role = 'ID')
 
 recipe_1 %>% prep() %>% summary()
 recipe_2 %>% prep() %>% summary()
+
+recipe_1_SAL %>% prep() %>% summary()
+recipe_2_SAL %>% prep() %>% summary()
 
 # 6.0 Models ---- 
 
@@ -79,11 +126,22 @@ wflw_fit_prophet<- workflow() %>%
                    add_recipe(recipe_1) %>%
                    fit(training(splits))
 
+wflw_fit_prophet_SAL<- workflow() %>% 
+                       add_model(prophet_reg() %>% set_engine('prophet')) %>%
+                       add_recipe(recipe_1_SAL) %>%
+                       fit(training(splits_SAL))
+
+
 # XGBoost
 wflw_fit_xgboost<- workflow() %>% 
                    add_model(boost_tree() %>% set_engine('xgboost')) %>%
                    add_recipe(recipe_2) %>%
                    fit(training(splits))
+
+wflw_fit_xgboost_SAL<- workflow() %>% 
+                       add_model(boost_tree() %>% set_engine('xgboost')) %>%
+                       add_recipe(recipe_2_SAL) %>%
+                       fit(training(splits_SAL))
 
 # Random Forest
 wflw_fit_rf<- workflow() %>% 
@@ -91,11 +149,22 @@ wflw_fit_rf<- workflow() %>%
               add_recipe(recipe_2) %>%
               fit(training(splits))
 
+
+wflw_fit_rf_SAL<- workflow() %>% 
+                  add_model(rand_forest() %>% set_engine('ranger')) %>%
+                  add_recipe(recipe_2_SAL) %>%
+                  fit(training(splits_SAL))
+
 # SVM 
 wflw_fit_svm<- workflow() %>% 
                add_model(svm_rbf() %>% set_engine('kernlab')) %>%
                add_recipe(recipe_2) %>%
                fit(training(splits))
+
+wflw_fit_svm_SAL<- workflow() %>% 
+                   add_model(svm_rbf() %>% set_engine('kernlab')) %>%
+                   add_recipe(recipe_2_SAL) %>%
+                   fit(training(splits_SAL))
 
 # Prophet Boost
 wflw_fit_prophet_boost<- workflow() %>% 
@@ -105,33 +174,66 @@ wflw_fit_prophet_boost<- workflow() %>%
                          add_recipe(recipe_1) %>%
                          fit(training(splits))
 
+wflw_fit_prophet_boost_SAL<- workflow() %>% 
+                             add_model(prophet_boost( seasonality_daily = FALSE, 
+                                                      seasonality_weekly = FALSE, 
+                                                      seasonality_yearly = FALSE) %>% set_engine('prophet_xgboost')) %>%
+                             add_recipe(recipe_1_SAL) %>%
+                             fit(training(splits_SAL))
+
 # 7.0 Modeltime workflow ----
 submodels_tbl<- modeltime_table(wflw_fit_prophet,
                                 wflw_fit_xgboost,
                                 wflw_fit_rf,
                                 wflw_fit_svm,
                                 wflw_fit_prophet_boost)
+
+submodels_tbl_SAL<- modeltime_table(wflw_fit_prophet_SAL,
+                                    wflw_fit_xgboost_SAL,
+                                    wflw_fit_rf_SAL,
+                                    wflw_fit_svm_SAL,
+                                    wflw_fit_prophet_boost_SAL)
+
 # Calibrate testing data
 submodel_calibrate_tbl<- submodels_tbl %>% modeltime_calibrate(testing(splits))
 
+submodel_calibrate_tbl_SAL<- submodels_tbl_SAL %>% modeltime_calibrate(testing(splits_SAL))
+
 # Measure Accuracy
 submodel_calibrate_tbl %>% modeltime_accuracy()
+submodel_calibrate_tbl_SAL %>% modeltime_accuracy()
 
 #Visualize test forecast
 submodel_calibrate_tbl %>% modeltime_forecast(new_data = testing(splits),
                                               actual_data = train_LT2_tbl,
                                               keep_data = TRUE) %>%
                            plot_modeltime_forecast()
+
+submodel_calibrate_tbl_SAL %>% modeltime_forecast(new_data = testing(splits_SAL),
+                                                  actual_data = train_SAL_tbl,
+                                                  keep_data = TRUE) %>%
+                               plot_modeltime_forecast()
+
+
 #Refit
 
 submodels_refit_tbl<- submodel_calibrate_tbl %>%
                       modeltime_refit(train_LT2_tbl)
+
+submodels_refit_tbl_SAL<- submodel_calibrate_tbl_SAL %>%
+                          modeltime_refit(train_SAL_tbl)
 
 # Visualize refitted model
 submodels_refit_tbl %>% modeltime_forecast(new_data = future_LT2_tbl,
                                            actual_data = train_LT2_tbl,
                                            keep_data = TRUE)%>%
                         plot_modeltime_forecast()
+
+submodels_refit_tbl_SAL %>% modeltime_forecast(new_data = future_SAL_tbl,
+                                               actual_data = train_SAL_tbl,
+                                               keep_data = TRUE)%>%
+                            plot_modeltime_forecast()
+
 
 # 8.0 Ensemble models
 ensemble_fit_mean<- submodels_tbl %>%
@@ -154,3 +256,18 @@ ensemble_refit_tbl %>% modeltime_forecast(new_data = future_LT2_tbl,
                                           actual_data = train_LT2_tbl,
                                           keep_data = TRUE) %>%
                       plot_modeltime_forecast()
+
+#SAl ensemble
+ensemble_fit_mean_SAL<- submodels_tbl_SAL %>%
+                        filter(!.model_id %in% c(4,5)) %>%
+                        ensemble_average(type = 'mean')
+
+ensemble_tbl_SAL<- modeltime_table(ensemble_fit_mean_SAL)
+
+ensemble_tbl_SAL %>% combine_modeltime_tables(submodels_tbl_SAL) %>%
+                     modeltime_accuracy(testing(splits_SAL))
+
+ensemble_tbl_SAL %>% modeltime_forecast(new_data = testing(splits_SAL),
+                                        actual_data = train_SAL_tbl,
+                                        keep_data = TRUE) %>%
+                     plot_modeltime_forecast()
